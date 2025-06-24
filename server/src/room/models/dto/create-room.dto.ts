af4a7a6461e0a40140037110f0c9aff268cbcd10
ZuevaDarya <zuevadasha55@gmail.com>
2025-06-24 15:34:35 +0300

    refactor: почистила dto файл

--- server/src/room/models/dto/create-room.dto.ts
+++ server/src/room/models/dto/create-room.dto.ts
@@ -1,20 +1,7 @@
 import { IsNotEmpty, IsUUID } from 'class-validator';
 
 export class CreateRoomDTO {
-  // @IsString()
-  // @Length(8, 8)
-  // @IsNotEmpty()
-  // readonly roomId: string;
-
   @IsUUID(4)
   @IsNotEmpty()
   creatorId: string;
-
-  //   @IsUUID(4)
-  //   @IsOptional()
-  //   playerId?: string;
-
-  //   @IsEnum(RoomStatus)
-  //   @IsNotEmpty()
-  //   status: TRoomStatus = RoomStatus.Waiting;
 }
